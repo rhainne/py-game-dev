e4d9b9fc1c5ba27e7901f10b5a40cdc2a10129a6
rain_ <rhinezen@gmail.com>
2017-05-13 03:52:50 +0200

    daily commit

--- keyboard_event_handler.py
+++ keyboard_event_handler.py
@@ -3,28 +3,48 @@ import pygame
 from pygame.locals import *
 from sys import exit
 
-background_image_filename = 'sushiplate.jpg'
+background_image_filename = './assets/Valkyrie_Wallpaper.jpg'
 
 pygame.init()
 
-screen = pygame.display.set_mode((640, 480), 0, 32)
+# screen = pygame.display.set_mode((640, 480), 0, 32)
+screen = pygame.display.set_mode((1920, 1080), FULLSCREEN, 32)
 background = pygame.image.load(background_image_filename).convert()
 
 x, y = 0, 0
 move_x, move_y = 0, 0
+
+# Following line make pygame ignore mouse motion
+pygame.event.set_blocked(MOUSEMOTION)
+
+# pygame.event.set_blocked([KEYDOWN, KEYUP]) # set_blocked also works for specific keys
+# pygame.event.set_blocked(None) # You can also unblock all events
+# pygame.event.set_allowed(None) # Or block them
+
+my_event = pygame.event.Event(KEYDOWN, key=K_SPACE, mod=0, unicode=u' ') # you can also pass as dictionary
+pygame.event.post(my_event)
+
+CATONKEYBOARD = USEREVENT+1
+my_event = pygame.event.Event(CATONKEYBOARD, message="Bad cat!")
+pygame.event.post(my_event)
+
+print(pygame.display.list_modes())
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
+        if event.type == CATONKEYBOARD:
+            print(event.message)
         if event.type == KEYDOWN:
-            if event.key == K_LEFT:
+            if event.key == K_LEFT or event.key == K_a:
                 move_x = -1
-            elif event.key == K_RIGHT:
+            elif event.key == K_RIGHT or event.key == K_d:
                 move_x = +1
-            elif event.key == K_UP:
+            elif event.key == K_UP or event.key == K_w:
                 move_y = -1
-            elif event.key == K_DOWN:
+            elif event.key == K_DOWN or event.key == K_s:
                 move_y = +1
         elif event.type == KEYUP:
             if event.key == K_LEFT:
@@ -35,6 +55,10 @@ while True:
                 move_y = 0
             elif event.key == K_DOWN:
                 move_y = 0
+            if event.key == K_ESCAPE:
+                pygame.quit()
+                exit()
+
         x+= move_x
         y+= move_y
 

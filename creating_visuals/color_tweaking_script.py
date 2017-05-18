5b0d246f14c97eef40168a67e2c6f08a7f48fbec
Adrian Garcia <Adrian Garcia>
2017-05-18 13:01:50 +0200

    color_tweaking_script.py cleaning

--- creating_visuals/color_tweaking_script.py
+++ creating_visuals/color_tweaking_script.py
@@ -6,6 +6,7 @@ from sys import exit
 l_width = 1280
 l_height = 800
 SCREEN_SIZE = (l_width, l_height)
+SCALE_HEIGHT = 100
 
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE, 0, 32)
@@ -13,9 +14,9 @@ screen = pygame.display.set_mode(SCREEN_SIZE, 0, 32)
 
 # Creates images with smooth gradients
 def create_scales(height):
-    red_scale_surface = pygame.surface.Surface(SCREEN_SIZE)
-    green_scale_surface = pygame.surface.Surface(SCREEN_SIZE)
-    blue_scale_surface = pygame.surface.Surface(SCREEN_SIZE)
+    red_scale_surface = pygame.surface.Surface((l_width, height))
+    green_scale_surface = pygame.surface.Surface((l_width, height))
+    blue_scale_surface = pygame.surface.Surface((l_width, height))
     for x_coord in range(l_width):
         c = int((x_coord/float(l_width - 1)) * 255.)
         red = (c, 0, 0)
@@ -27,7 +28,7 @@ def create_scales(height):
         pygame.draw.rect(blue_scale_surface, blue, line_rect)
     return red_scale_surface, green_scale_surface, blue_scale_surface
 
-red_scale, green_scale, blue_scale = create_scales(100)
+red_scale, green_scale, blue_scale = create_scales(SCALE_HEIGHT)
 color = [127, 127, 127]
 
 while True:
@@ -43,24 +44,24 @@ while True:
     screen.fill((0, 0, 0))
 
     # Draw the scales to the screen
-    screen.blit(red_scale, (0, 00))
-    screen.blit(green_scale, (0, 100))
-    screen.blit(blue_scale, (0, 200))
+    screen.blit(red_scale, (0, SCALE_HEIGHT * 0))
+    screen.blit(green_scale, (0, SCALE_HEIGHT * 1))
+    screen.blit(blue_scale, (0, SCALE_HEIGHT * 2))
 
     x, y = pygame.mouse.get_pos()
 
     # If the mouse was pressed on one of the sliders, adjust the color component
     if pygame.mouse.get_pressed()[0]:
         for component in range(3):
-            if y > component * 100 and y < (component + 1) * 100:
+            if y > component * SCALE_HEIGHT and y < (component + 1) * SCALE_HEIGHT:
                 color[component] = int((x / float(l_width - 1)) * 255.)
             pygame.display.set_caption("PyGame Color Test - " + str(tuple(color)))
 
     # Draw a circle for each slider to represent the current setting
     for component in range(3):
-        pos = (int((color[component]/255.) * float(l_width - 1)), component * 100 + 40)
+        pos = (int((color[component] / 255.) * float(l_width - 1)), component * SCALE_HEIGHT + 40)
         pygame.draw.circle(screen, (255, 255, 255), pos, 20)
 
-    pygame.draw.rect(screen, tuple(color), (0, 300, l_width, 300))
+    pygame.draw.rect(screen, tuple(color), (0, SCALE_HEIGHT * 3, l_width, SCALE_HEIGHT * 3))
 
     pygame.display.update()
